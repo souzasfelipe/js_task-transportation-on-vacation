72acc067358170eb1155c793e9c52a1b60353cfc
Your Name <lipe291@gmail.com>
2025-03-22 09:13:06 -0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
+
+  if (days < 3) {
+    return 80;
+  }
+
+  if ((days === 3) & (days < 6)) {
+    return 100;
+  }
+
+  if ((days > 3) & (days === 6)) {
+    return 220;
+  }
+
+  if (days >= 7) {
+    return 230;
+  }
 }
 
 module.exports = calculateRentalCost;
